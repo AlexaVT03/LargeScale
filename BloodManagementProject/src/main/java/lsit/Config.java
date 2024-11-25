bc9fa3f92323956209f4a3fe8864ac56b8ea3d25
Eric Weindorfer <wei17083@spengergasse.at>
2024-11-25 20:33:30 +0100

    Commented out role restrictions for now, as to not interfere with development

--- BloodManagementProject/src/main/java/lsit/Config.java
+++ BloodManagementProject/src/main/java/lsit/Config.java
@@ -26,12 +26,16 @@ public class Config{
             .authorizeHttpRequests(authorize -> authorize
                 //authorizations currently non-sensical, just for testing purposes
                 .requestMatchers("/").permitAll() //lets anyone access home page
+
+                /* Commented role restrictions as to not interfere with further development
                 .requestMatchers(HttpMethod.GET, "/donors/listall").hasAnyRole("Fail_test","DONOR") //hasAnyRole when multiple roles have access
                 .requestMatchers(HttpMethod.GET, "/eligibility-forms/listall").hasRole("MEDICAL-STAFF")
                 .requestMatchers(HttpMethod.GET, "/blood-samples/listall").hasRole("BLOOD-MANAGEMENT-UNIT")
                 .requestMatchers(HttpMethod.GET, "/health-checks/listall").hasRole("FAIL_TEST") //test; should be forbidden
-                .anyRequest().authenticated() //all other pages you just need to be logged in; no role requirement
+                */
                 //TODO: add role restrictions for every call/page
+
+                .anyRequest().authenticated() //all other pages you just need to be logged in; no role requirement
             );
 
         return http.build();
